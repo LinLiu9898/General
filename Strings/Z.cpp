1a924e29eda10d366b54ed74ab69cc99f01394a1
Qualified <60632019+LinLiu9898@users.noreply.github.com>
2021-03-11 17:54:36 -0600

    fix

--- Strings/Z.cpp
+++ Strings/Z.cpp
@@ -1,7 +1,7 @@
 vector<int> z(string s) {
 	int n = si(s);
 	vector<int> z(n);
-	int x = 0. y = 0;
+	int x = 0, y = 0;
 	z[0] = si(s);
 	for(int i = 1; i < n; ++i) {
 		z[i] = max(0LL, min(z[i - x], y - i + 1));
