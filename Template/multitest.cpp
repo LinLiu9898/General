d00da88536e1c0163bb17ee075ee8fa1c80fcaba
Qualified <60632019+LinLiu9898@users.noreply.github.com>
2021-03-15 08:23:47 -0500

    Create multitest.cpp

---
+++ Template/multitest.cpp
@@ -0,0 +1,34 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+#define int long long
+#define trav(i, a) for(auto& i: a)
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define si(a) ((int)(a).size())
+#define ins insert
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+
+const int MOD = 1e9 + 7;
+const int INF = 1e18;
+const string nl = "\n";
+
+
+
+void solve() {
+	
+}
+
+int32_t main() {
+	ios::sync_with_stdio(0);
+	cin.tie(nullptr);
+
+	int T;
+	cin >> T;
+	while(T--) {
+		solve();
+	}
+}
