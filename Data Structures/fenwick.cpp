e17d3fddb67684d3974e1725f1bd5b24b6c75436
Qualified <60632019+LinLiu9898@users.noreply.github.com>
2021-03-14 21:23:01 -0500

    fix

--- Data Structures/fenwick.cpp
+++ Data Structures/fenwick.cpp
@@ -1,32 +1,32 @@
 template <class T>
 struct fenwick {
 	int n;
-	vector<int> tree;
-	fenwick(const vector<int>& a) {
+	vector<T> tree;
+	fenwick(const vector<T>& a) {
 		n = si(a);
 		tree.resize(n + 2);
 		for(int i = 0; i < si(a); ++i) {
 			update(i, a[i]);
 		}
 	}
-	void update(int pos, int dif) {
+	void update(int pos, T dif) {
 		++pos;
 		for(int i = pos; i <= n; i += (i & -i)) {
 			tree[i] += dif;
 		}
 	}
-	void set(int pos, int val) {
+	void set(int pos, T val) {
 		update(pos, val - query(pos, pos));
 	}
-	int query(int l) {
+	T query(int l) {
 		++l;
-		int sum = 0;
+		T sum = 0;
 		for(int i = l; i > 0; i -= (i & -i)) {
 			sum += tree[i];
 		}
 		return sum;
 	}
-	int query(int l, int r) {
+	T query(int l, int r) {
 		return query(r) - query(l - 1);
 	}
 };
