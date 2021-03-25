1f23abbf13605a3d70bddb1163ef16ac037ccead
Qualified <60632019+LinLiu9898@users.noreply.github.com>
2021-03-25 10:13:29 -0500

    Added struct item

--- Data Structures/segtree.cpp
+++ Data Structures/segtree.cpp
@@ -1,12 +1,22 @@
+struct item {
+
+	static item neutral() {
+
+	}
+	template <class T>
+	void init(T x) {
+
+	}
+	static item merge(item a, item b) {
+
+	}
+};
+
 template <class T>
 struct segtree {
 	int n = 1;
-	vector<T> tree;
-	const T unit = 0LL;
-	// comb(a, unit) = a
-	T comb(T a, T b) {
-		return a + b;
-	}
+	vector<item> tree;
+	const T unit = -1;
 	segtree(vector<T> a) {
 		init(a);
 	}
@@ -20,31 +30,36 @@ struct segtree {
 		assert(si(a) == n);
 		tree.resize(2 * n + 2);
 		for(int i = n - 1; i < 2 * n - 1; ++i) {
-			tree[i] = a[i - n + 1];
+			tree[i].init(a[i - n + 1]);
 		}
 		for(int i = n - 2; i >= 0; --i) {
-			tree[i] = comb(tree[i * 2 + 1], tree[i * 2 + 2]);
+			tree[i] = item::merge(tree[i * 2 + 1], tree[i * 2 + 2]);
 		}
 	}
 	void set(int pos, T val) {
 		pos += n - 1;
-		tree[pos] = val;
+		tree[pos].init(val);
 		while(pos) {
 			pos = (pos - 1) / 2;
-			tree[pos] = comb(tree[pos * 2 + 1], tree[pos * 2 + 2]);
+			tree[pos] = item::merge(tree[pos * 2 + 1], tree[pos * 2 + 2]);
 		}
 	}
-	T query(int lx, int rx, int l, int r, int node) {
+	item query(int lx, int rx, int l, int r, int node) {
 		if(lx >= l && rx <= r) {
 			return tree[node];
 		}
 		if(rx < l || lx > r) {
-			return unit;
+			return item::neutral();
 		}
 		int mid = (lx + rx) / 2;
-		return comb(query(lx, mid, l, r, node * 2 + 1), query(mid + 1, rx, l, r, node * 2 + 2));
+		return item::merge(query(lx, mid, l, r, node * 2 + 1), query(mid + 1, rx, l, r, node * 2 + 2));
 	}
-	T query(int l, int r) {
+	item query(int l, int r) {
+		l = max(l, 0LL);
+		r = min(r, n - 1);
+		if(l > r) {
+			return item();
+		}
 		return query(0, n - 1, l, r, 0);
 	}
 };
