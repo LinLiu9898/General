381d4cf5e6203b98f88b888037d0310d44acb479
Qualified <60632019+LinLiu9898@users.noreply.github.com>
2021-04-11 19:44:19 -0500

    Spacing

--- Data Structures/fenwick.cpp
+++ Data Structures/fenwick.cpp
@@ -2,19 +2,23 @@ template <class T>
 struct fenwick {
 	int n;
 	vector<T> tree;
-	const T ID = 0LL;
+	const T unit = 0LL;
+
 	fenwick(const int& _n) {
 		n = _n;
-		tree.assign(n + 2, ID);
+		tree.assign(n + 2, unit);
 	}
+
 	void add(int pos, T dif) {
 		for(int i = pos + 1; i <= n; i += (i & -i)) {
 			tree[i] += dif;
 		}
 	}
+
 	void set(int pos, T val) {
 		add(pos, val - query(pos, pos));
 	}
+
 	T query(int l) {
 		T sum = 0;
 		for(int i = l + 1; i > 0; i -= (i & -i)) {
@@ -22,6 +26,7 @@ struct fenwick {
 		}
 		return sum;
 	}
+
 	T query(int l, int r) {
 		return query(r) - query(l - 1);
 	}
