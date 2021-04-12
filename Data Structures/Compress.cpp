76a420456c9e02ee4ba2863d41c7a38ec660f043
Qualified <60632019+LinLiu9898@users.noreply.github.com>
2021-04-11 19:45:55 -0500

    Spacing

--- Data Structures/Compress.cpp
+++ Data Structures/Compress.cpp
@@ -5,6 +5,7 @@ void compress(const vector<int>& x) {
 	sort(all(coord_compress));
 	coord_compress.resize(unique(all(coord_compress)) - coord_compress.begin());
 }
+
 int get(int x) {
 	int pos = lower_bound(all(coord_compress), x) - coord_compress.begin();
 	assert(coord_compress[pos] == x);
