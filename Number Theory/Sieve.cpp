6caabafb0e316d2cc706b380738b0d0d16e9ba8d
Qualified <60632019+LinLiu9898@users.noreply.github.com>
2021-04-19 09:59:06 -0500

    Added cano function, also changed factor function

--- Number Theory/Sieve.cpp
+++ Number Theory/Sieve.cpp
@@ -1,6 +1,7 @@
 int cur_generated;
 vector<bool> isprime;
 vector<int> primes, spf;
+bool is_built_sieve = false;
 
 void sieve(int maximum) {
 	cur_generated = maximum;
@@ -24,14 +25,51 @@ void sieve(int maximum) {
 			primes.pb(i);
 		}
 	}
+	is_built_sieve = true;
 }
 
-vector<int> factor(int x) {
-	assert(x > 0 && cur_generated >= x);
-	vector<int> res;
-	while(x > 1) {
-		res.pb(spf[x]);
-		x /= spf[x];
+vector<array<int, 2>> factor(int n) {
+	assert(is_built_sieve);
+	int cur_ma = cur_generated;
+	assert(n > 0 && n <= cur_ma * cur_ma);
+	vector<array<int, 2>> res;
+	if(n <= cur_ma) {
+		while(n > 1) {
+			int p = spf[n], expo = 0;
+			do {
+				n /= p;
+				++expo;
+			} while(n % p == 0);
+			res.pb({p, expo});
+		}
+		return res;
+	}
+	trav(p, primes) {
+		if(p * p > n) {
+			break;
+		}
+		if(n % p == 0) {
+			res.pb({p, 0});
+			do {
+				n /= p;
+				++res.back()[1];
+			} while(n % p == 0);
+		}
+	}
+	if(n > 1) {
+		res.pb({n, 1});
 	}
 	return res;
+}
+
+int cano(int x) {
+	assert(is_built_sieve);
+	vector<array<int, 2>> prime_fac = factor(x);
+	int prod = 1;
+	trav(i, prime_fac) {
+		if(i[1] % 2 != 0) {
+			prod *= i[0];
+		}
+	}
+	return prod;
 }
