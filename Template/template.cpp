d09d744e2ff359795f1d7ef0d680b4e18b16aaf5
Qualified <60632019+LinLiu9898@users.noreply.github.com>
2021-04-25 19:48:25 -0500

    Update template.cpp

--- Template/template.cpp
+++ Template/template.cpp
@@ -1,7 +1,7 @@
 #include "bits/stdc++.h"
 using namespace std;
 
-#define int long long
+#define int int64_t
 #define trav(i, a) for(auto& i: a)
 #define all(a) a.begin(), a.end()
 #define rall(a) a.rbegin(), a.rend()
